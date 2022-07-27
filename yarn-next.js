4e389c448606648f1695f2dd0b3056b0429d94e0
Rossy-Muhamad <109084012+Rossy-Muhamad@users.noreply.github.com>
2022-07-27 08:32:08 +0000

    Update yarn-next.js

--- yarn-next.js
+++ yarn-next.js
@@ -2,3 +2,10 @@ install yarn  : npm -g install yarn
 chek version  : yarn -v
 install yarn react : yarn create react-app my-app
 install npm react : npm init react-app my-app
+
+# Install css framewrok 
+- npm i react-router-dom axios bulma
+
+# Install auto refresh
+- npm install -g nodemon
+- nodemon file
